e44a9acc7baa0c4a85fbff12d1a0e7f5593bf5f4
Sajad Salehi <102877977+Sajad-Salehi@users.noreply.github.com>
2022-06-27 14:30:15 +0430

    Add files via upload

--- settings.py
+++ settings.py
@@ -3,5 +3,11 @@ class Settings:
     def __init__(self):
 
         self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_height = 700
         self.bg_color = (137, 207, 240)
+        self.ship_speed = 1.5
+
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60 ,60)
