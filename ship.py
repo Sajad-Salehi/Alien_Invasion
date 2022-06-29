ed03bd63e1d542c543a27af4177d550330c05c2d
Sajad Salehi <102877977+Sajad-Salehi@users.noreply.github.com>
2022-06-30 01:02:27 +0430

    Add files via upload

--- ship.py
+++ ship.py
@@ -1,15 +1,17 @@
 import pygame 
+from pygame.sprite import Sprite
 
-class Ship:
+class Ship(Sprite):
 
     def __init__(self, ai_game):
-
+        
+        super().__init__()
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen_rect = ai_game.screen.get_rect()
         self.settings = ai_game.settings
 
-        self.image = pygame.image.load('images/logo1.jpeg')
-        self.image = pygame.transform.scale(self.image, (70, 70))
+        self.image = pygame.image.load('images/logo5.jpg')
+        self.image = pygame.transform.scale(self.image, (90, 80))
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
 
@@ -29,3 +31,8 @@ class Ship:
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+
